54d6b211fb082ff7e3de3a6d4a20ff25b44bdf01
IrakliGiorgadze <ika.giorgadze@gmail.com>
2022-05-23 00:33:58 +0400

    final solution

--- cache.go
+++ cache.go
@@ -1,23 +1,59 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type cacheItem struct {
+	value          string
+	expirationTime time.Time
+}
+
+func (c cacheItem) isExpired() bool {
+	return c.expirationTime != time.Time{} && c.expirationTime.Unix() < time.Now().Unix()
+}
 
 type Cache struct {
+	items map[string]cacheItem
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		items: map[string]cacheItem{},
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	item, ok := c.items[key]
+	if !ok || item.isExpired() {
+		return "", false
+	}
 
+	return item.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.PutTill(key, value, time.Time{})
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	result := make([]string, 0)
+	for key, item := range c.items {
+		if !item.isExpired() {
+			result = append(result, key)
+		}
+	}
+
+	if len(result) == 0 {
+		return nil
+	}
+
+	return result
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.items[key] = cacheItem{
+		value:          value,
+		expirationTime: deadline,
+	}
 }
